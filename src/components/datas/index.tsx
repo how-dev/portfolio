024d0811d5c92d4b0b361c61b7aa8c3cc591d483
HowardRicardo <howard.medeiros@gmail.com>
2021-01-04 13:54:48 -0300

    feature: about me

--- src/components/datas/index.tsx
+++ src/components/datas/index.tsx
@@ -14,7 +14,7 @@ export const Datas: React.FC = () => {
         <div className="dataBox">
             <img alt="profile" src={profilePhoto} className="profilePhoto" />
             <h1 style={{color: "#C0E0DE"}}>Howard Ricardo</h1>
-            <span style={{color: "#C0E0DE"}}>HTML, CSS, JavaScript, TypeScript, React, Redux-thunk, Git, Scrum</span>
+            <span style={{color: "#C0E0DE"}}>HTML5, CSS3, JavaScript (ES6+), TypeScript, React, Redux-Thunk, Git, Scrum</span>
             <div className="contacts">
                 <h1 style={{color: "#C0E0DE", margin: 0, marginBottom: 20}}>Contatos:</h1>
                 <a className="link" href="https://www.linkedin.com/in/how-dev/" rel="noreferrer" target="_blank"><LinkedInIcon/>Linkedin: @how-dev </a>
@@ -25,7 +25,23 @@ export const Datas: React.FC = () => {
                 <span className="linkEmail" ><EmailIcon/>Email: howard.medeiros@gmail.com</span>
                 <img alt="instagramQR" src={instagramQR} className="instagramQR" />
             </div>
-
+            <div className="aboutMe">
+                <h1 style={{color: "#C0E0DE", margin: 0, marginBottom: 20}}>Quem sou eu:</h1>
+                <p className="bodyAboutMe">
+                    &nbsp;&nbsp; Desde criança eu sempre fui apaixonado por programação e tecnologia.
+                    Meu pai trabalha com computação e sempre me ensinou lógica de programação desde pequeno.
+                    Aos 7 anos eu já programava jogos em RPG Maker VX/XP. Na minha adolescência, trabalhei
+                    com amigos em jogos online inspirados em Habbo Hotel, que usávamos Flash, da Adobe, e C#.
+                    Na vida adulta, ao entrar na faculdade de Engenharias, tive contato com Python, e logo com
+                    JavaScript. Hoje eu tenho 20 anos, e minha especialidade é Desenvolvimento Web em React (principal)
+                    , Angular, Next e Vue.
+                </p>
+                <h3 style={{color: "#C0E0DE"}}>Metas para o futuro: </h3>
+                <p className="bodyAboutMe">
+                    &nbsp;&nbsp; Hoje eu sou um Desenvolvedor Front-End, mas eu estou em busca de me tornar um Full-Stack,
+                    estudando Python, DJANGO, Flask, MySQL, PHP, GraphQL e API REST.
+                </p>
+            </div>
         </div>
     )
 }
